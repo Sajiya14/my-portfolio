1b2417f35d8d9dec672bc40bccce4d8c4856f9ff
Sajiya14 <sajithperera215@gmail.com>
2026-04-02 23:43:10 +0530

    fix cors origin

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://my-portfolio-zp42.vercel.app/"
+    "https://my-portfolio-zp42.vercel.app"
   ],
   methods: ["GET", "POST"],
 }));
