dfa8f80a669a7de9712d790fc2d8c61b3ed909be
Sajiya14 <sajithperera215@gmail.com>
2026-08-31 21:42:37 +0530

    update project structure in projectData file

--- frontend/src/pages/Projects/ProjectData.jsx
+++ frontend/src/pages/Projects/ProjectData.jsx
@@ -1,16 +1,17 @@
 import TfsThumbnail from "../../assets/Thumbnail/TFS Thumbnail.png";
 import RitThumbnail from "../../assets/Thumbnail/Royal IT.png";
+import EoDThumbnail from "../../assets/Thumbnail/Eod Thumbnail.png";
 
 const projectData = [
   {
     id: 1,
-    title: "Royal Information Technology (pvt) Ltd Website Design",
+    title: "Department of Examinations - Sri Lanka Website Redesign Concept",
     category: "UX/UI",
-    thumbnailImg: RitThumbnail,
-    projectType: "Company Portfolio Website",
-    description: "A corporate website design for RIT Company to showcase their services and company profile in a modern and user-friendly UI design.",
+    thumbnailImg: EoDThumbnail,
+    projectType: "Landing Page Redesign Concept",
+    description: "A landing page redesign concept for the Department of Examinations Sri Lanka, focused on improving service discoverability, information hierarchy, and user experience through a modern and accessible UI design.",
     tags: ["Figma", "UI Design", "Website Design"],
-    link: "/royalItUI"
+    link: "/EoDUI"
   },
   {
     id: 2,
@@ -21,6 +22,16 @@ const projectData = [
     description: "A modern UI design for the Uni-T Fashion E-commerce website, focusing on a clean, responsive, and user-friendly interface.",
     tags: ["Figma", "UI Design", "E-commerce Design"],
     link: "/TfsUi"
+  },
+  {
+    id: 3,
+    title: "Royal Information Technology (pvt) Ltd Website Design",
+    category: "UX/UI",
+    thumbnailImg: RitThumbnail,
+    projectType: "Company Portfolio Website",
+    description: "A corporate website design for RIT Company to showcase their services and company profile in a modern and user-friendly UI design.",
+    tags: ["Figma", "UI Design", "Website Design"],
+    link: "/royalItUI"
   },
 ];
 
